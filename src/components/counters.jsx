d772b04366cd2ff78c4b45ac30070d350c2bf541
eric babcock <eric.babcock@qurasoft.de>
2023-04-14 12:06:16 +0200

    destructured all properties

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Counter from './counter'
 
-const Counters = (props) => {
+const Counters = ({onReset,onIncrement,onDelete,counters}) => {
   return (
     <>
       <button
-        onClick={props.onReset}
+        onClick={onReset}
         className='btn btn-primary btn-sm m-2'>
         Reset
       </button>
 
-      {props.counters.map(counter => (
+      {counters.map(counter => (
         <Counter id={ counter.id }
                  key={ counter.id }
-                 onDelete={ props.onDelete }
-                 onIncrement={ props.onIncrement }
+                 onDelete={ onDelete }
+                 onIncrement={ onIncrement }
                  counter={counter}/>
 
       ))}
