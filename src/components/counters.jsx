765e643999cb6eb6e3a3537186af8d2a8db4c12b
eric babcock <eric.babcock@qurasoft.de>
2023-04-14 11:54:49 +0200

    converted Object to stateless functional component.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,26 +1,27 @@
-import React, {Component, createContext} from "react";
+import React from "react";
 import Counter from './counter'
-class Counters extends Component {
 
-  render() {
-    return (
-      <>
-        <button
-          onClick={this.props.onReset}
-          className='btn btn-primary btn-sm m-2'>
-          Reset
-        </button>
+const Counters = (props) => {
+  return (
+    <>
+      <button
+        onClick={props.onReset}
+        className='btn btn-primary btn-sm m-2'>
+        Reset
+      </button>
 
-        {this.props.counters.map(counter => (
-          <Counter id={ counter.id }
-                   key={ counter.id }
-                   onDelete={ this.props.onDelete }
-                   onIncrement={ this.props.onIncrement }
-                   counter={counter}/>
+      {props.counters.map(counter => (
+        <Counter id={ counter.id }
+                 key={ counter.id }
+                 onDelete={ props.onDelete }
+                 onIncrement={ props.onIncrement }
+                 counter={counter}/>
 
-        ))}
-      </>)
-  }
+      ))}
+    </>
+  )
 }
+
+
 
 export default Counters
