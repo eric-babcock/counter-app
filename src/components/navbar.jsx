d53b64a607c06f1c2b86871c160f5c2aa91cdc9b
eric babcock <eric.babcock@qurasoft.de>
2023-04-14 12:02:24 +0200

    destructured totalCounters property

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const NavBar = (props) => {
+const NavBar = ({totalCounters}) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="reddit.com">
         Navbar
         <span className='badge bg-pill bg-secondary m-2'>
-            {props.totalCounters}
+            {totalCounters}
           </span>
       </a>
     </nav>
